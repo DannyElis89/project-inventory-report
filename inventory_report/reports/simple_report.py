aee3893cf2bccfc9d34c475f2471b3192b59af5e
DannyElis89 <dannyelis89@gmail.com>
2023-02-12 19:56:37 -0300

    Requisito 02

--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -1,2 +1,37 @@
+class ReportFilter:
+    @classmethod
+    def data_de_fabricacao(cls, report):
+        lista_data_de_fabricacao = []
+        for date in report:
+            lista_data_de_fabricacao.append(date['data_de_fabricacao'])
+
+        return min(lista_data_de_fabricacao)
+
+    @classmethod
+    def data_de_validade(cls, report):
+        lista_data_de_validade = []
+        for date in report:
+            lista_data_de_validade.append(date['data_de_validade'])
+
+        return min(lista_data_de_validade)
+
+    @classmethod
+    def empresa_com_mais_produtos(cls, report):
+        todas_empresas = []
+        for empresa in report:
+            todas_empresas.append(empresa['nome da empresa'])
+        return max(todas_empresas, key=todas_empresas.count)
+
+
 class SimpleReport:
-    pass
+    @classmethod
+    def generate(cls, report):
+        data_de_fabricacao = ReportFilter.data_de_fabricacao(report)
+        data_de_validade = ReportFilter.data_de_validade(report)
+        empresa = ReportFilter.empresa_com_mais_produtos(report)
+
+        return (
+            f'Data de fabricação mais antiga: {data_de_fabricacao}\n'
+            f'Data de validade mais próxima: {data_de_validade}\n'
+            f'Empresa com mais produtos: {empresa}'
+        )
