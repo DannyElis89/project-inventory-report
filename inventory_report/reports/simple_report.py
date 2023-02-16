48e96c396bc70b522a69688429e6a82f62c6e531
DannyElis89 <dannyelis89@gmail.com>
2023-02-16 14:55:57 -0300

    Requisito 02

--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -1,37 +1,35 @@
 class ReportFilter:
     @classmethod
     def data_de_fabricacao(cls, report):
-        lista_data_de_fabricacao = []
+        lista_datas = []
         for date in report:
-            lista_data_de_fabricacao.append(date['data_de_fabricacao'])
-
-        return min(lista_data_de_fabricacao)
+            lista_datas.append(date['data_de_fabricacao'])
+        return min(lista_datas)
 
     @classmethod
     def data_de_validade(cls, report):
-        lista_data_de_validade = []
+        lista_datas = []
         for date in report:
-            lista_data_de_validade.append(date['data_de_validade'])
-
-        return min(lista_data_de_validade)
+            lista_datas.append(date['data_de_validade'])
+        return min(lista_datas)
 
     @classmethod
-    def empresa_com_mais_produtos(cls, report):
-        todas_empresas = []
-        for empresa in report:
-            todas_empresas.append(empresa['nome da empresa'])
-        return max(todas_empresas, key=todas_empresas.count)
+    def nome_da_empresa(cls, report):
+        all_companies = []
+        for company in report:
+            all_companies.append(company['nome_da_empresa'])
+        return max(all_companies, key=all_companies.count)
 
 
 class SimpleReport:
     @classmethod
     def generate(cls, report):
-        data_de_fabricacao = ReportFilter.data_de_fabricacao(report)
-        data_de_validade = ReportFilter.data_de_validade(report)
-        empresa = ReportFilter.empresa_com_mais_produtos(report)
+        oldest_fabrication = ReportFilter.data_de_fabricacao(report)
+        closest_expiration = ReportFilter.data_de_validade(report)
+        company = ReportFilter.nome_da_empresa(report)
 
         return (
-            f'Data de fabricação mais antiga: {data_de_fabricacao}\n'
-            f'Data de validade mais próxima: {data_de_validade}\n'
-            f'Empresa com mais produtos: {empresa}'
+            f'Data de fabricação mais antiga: {oldest_fabrication}\n'
+            f'Data de validade mais próxima: {closest_expiration}\n'
+            f'Empresa com mais produtos: {company}'
         )
